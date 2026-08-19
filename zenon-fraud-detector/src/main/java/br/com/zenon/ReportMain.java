37ab93fda3b89fff87d220aa50a795c5439972a9
Gineude Silvestre <gineude@gmail.com>
2026-08-18 21:16:10 -0300

    Internacionalizacao do Relatorio

--- zenon-fraud-detector/src/main/java/br/com/zenon/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/ReportMain.java
@@ -1,10 +1,12 @@
 package br.com.zenon;
 
-import br.com.zenon.reports.Statistics;
 import br.com.zenon.reports.TransactionReport;
 
-import java.math.BigDecimal;
-import java.util.stream.Stream;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static java.text.MessageFormat.format;
 
 public class ReportMain {
 
@@ -14,10 +16,43 @@ public class ReportMain {
         var transactionReport = new TransactionReport();
         var st = transactionReport.readTransactionsLazyLoad(fileName);
 
-        System.out.printf("""
-                Total de linhas: %d
-                Total de fraudes: %d
-                Valor total transacionado: %.2f
-                %n""", st.totalTransactions(), st.totalFrauds(), st.totalAmount());
+        Locale localeUs = Locale.US;
+        Locale localePtBr = Locale.of("pt", "BR");
+
+        ResourceBundle bundleUs = ResourceBundle.getBundle("report", localeUs);
+        NumberFormat numberFormatUsMoeda = NumberFormat.getCurrencyInstance(localeUs);
+        NumberFormat numberFormatUsInteiro = NumberFormat.getNumberInstance(localeUs);
+
+        String totalAmount = numberFormatUsMoeda.format(st.totalAmount());
+        String totalFrauds = numberFormatUsInteiro.format(st.totalFrauds());
+        String totalTransactions = numberFormatUsInteiro.format(st.totalTransactions());
+
+        String totalLinhas = format(bundleUs.getString("total.de.linhas"), totalTransactions);
+        String totalFraudes = format(bundleUs.getString("total.de.fraudes"), totalFrauds);
+        String totalTransacionado = format(bundleUs.getString("valor.total.transacionado"), totalAmount);
+
+        System.out.println("Imprimindo em US");
+        System.out.println(totalLinhas);
+        System.out.println(totalFraudes);
+        System.out.println(totalTransacionado);
+
+        ResourceBundle bundlePtBr = ResourceBundle.getBundle("report", localePtBr);
+        NumberFormat numberFormatPtBrMoeda = NumberFormat.getCurrencyInstance(localePtBr);
+        NumberFormat numberFormatPtBrInteiro = NumberFormat.getNumberInstance(localePtBr);
+
+        String totalAmountPt = numberFormatPtBrMoeda.format(st.totalAmount());
+        String totalFraudsPt = numberFormatPtBrInteiro.format(st.totalFrauds());
+        String totalTransactionsPt = numberFormatPtBrInteiro.format(st.totalTransactions());
+
+        String totalLinhasPt = format(bundlePtBr.getString("total.de.linhas"), totalTransactionsPt);
+        String totalFraudesPt = format(bundlePtBr.getString("total.de.fraudes"), totalFraudsPt);
+        String totalTransacionadoPt = format(bundlePtBr.getString("valor.total.transacionado"), totalAmountPt);
+
+        System.out.println("Imprimindo em PT");
+        System.out.println(totalLinhasPt);
+        System.out.println(totalFraudesPt);
+        System.out.println(totalTransacionadoPt);
+
+
     }
 }
