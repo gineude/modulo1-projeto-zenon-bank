6bb35977f95c769feeb90baed9e4b5b23b32695f
Gineude Silvestre <gineude@gmail.com>
2026-08-20 16:24:54 -0300

    Criacao da classe EfficientTransactionIngestor

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/dao/EfficientTransactionIngestor.java
@@ -0,0 +1,12 @@
+package br.com.zenon.dao;
+
+import br.com.zenon.fraud.Transaction;
+
+import java.util.function.Consumer;
+
+public class EfficientTransactionIngestor {
+
+    public void readAsStream(String fileName, Consumer<Transaction> transactionConsumer) {
+
+    }
+}
